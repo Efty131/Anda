860a32afbc6549b8f890f8d71bc3e055e1887358
Efty <efty131331@gmail,com>
2021-10-26 13:01:42 +0600

    navbar fixed

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navberLinks = document.getElementsByClassName('naav-links')[0]
-
-toggleButton.addEventListener('click', () => {
-    navberLinks.classList.toggle('active')
-})
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = () => {
+  menu.classList.add("active");
+  menuBtn.classList.add("hide");
+}
+cancelBtn.onclick = () => {
+  menu.classList.remove("active");
+  menuBtn.classList.remove("hide");
+}
